5cad63efa4d98fcfa0d50819d4e7c00bf081151e
bus1hero <petru.cervac@gmail.com>
2024-12-26 22:14:35 +0200

    build: clean up nuke build folder

--- build/Configuration.cs
+++ build/Configuration.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using Nuke.Common.Tooling;
 
 [TypeConverter(typeof(TypeConverter<Configuration>))]
-public class Configuration : Enumeration
+public sealed class Configuration : Enumeration
 {
     public static Configuration Debug = new Configuration { Value = nameof(Debug) };
     public static Configuration Release = new Configuration { Value = nameof(Release) };
