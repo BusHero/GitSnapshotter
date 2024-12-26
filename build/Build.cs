26fabff7c125cdcf4683f9bb3a19fae47bdce85b
Cervac Petru <petru.cervac@gmail.com>
2024-12-26 23:38:15 +0200

    ci: upload artifact (#7)

--- build/Build.cs
+++ build/Build.cs
@@ -1,7 +1,13 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
+
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
@@ -16,9 +22,13 @@ sealed class Build : NukeBuild
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-    
-    [Solution(relativePath: "Source/GitSnapshotter.sln", GenerateProjects = true)] 
-    readonly Solution Solution;
+
+    [Solution(relativePath: "Source/GitSnapshotter.sln", GenerateProjects = true)]
+    readonly Solution Solution = null!;
+
+    [Parameter("Github Output")] private readonly string GithubOutput = null!;
+
+    [Parameter] public AbsolutePath PublishDirectory { get; } = null!;
 
     Target Clean => _ => _
         .Before(Restore)
@@ -47,16 +57,24 @@ sealed class Build : NukeBuild
         });
 
     Target Publish => _ => _
+        .Requires(() => PublishDirectory)
         .DependsOn(Compile)
-        .Executes(() =>
+        .Executes(async () =>
         {
             DotNetPublish(_ => _
                 .EnableNoLogo()
                 .EnableNoBuild()
                 .EnableNoRestore()
                 .SetConfiguration(Configuration)
-                .SetOutput(RootDirectory / "publish" / "GitSnapshotter.Console")
+                .SetOutput(PublishDirectory)
                 .SetProject(Solution.GitSnapshotter_Console)
             );
+            
+            await OutputToGithub("version", DateTime.Now.ToString("yyyyMMddHHmmss"));
         });
+
+    async Task OutputToGithub(string name, object content)
+    {
+        await File.AppendAllTextAsync(GithubOutput, $"{name}={content}\n");
+    }
 }
