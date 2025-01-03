8fc4f5cf20fc88cd7193bf4b2c1d4d9a4c7a49b5
bus1hero <petru.cervac@gmail.com>
2025-01-04 02:28:55 +0300

    feat: update interface for CreateGitRepository

--- Source/GitSnapshotter.UnitTests/GitTasks.cs
+++ Source/GitSnapshotter.UnitTests/GitTasks.cs
@@ -4,15 +4,13 @@ namespace GitSnapshotter.UnitTests;
 
 public static class GitTasks
 {
-    public static async Task<string> CreateGitRepository(string path)
+    public static async Task CreateGitRepository(string path)
     {
         await Cli.Wrap("git")
             .WithArguments(_ => _
                 .Add("init")
                 .Add(path))
             .ExecuteAsync();
-
-        return path;
     }
 
     public static async Task<bool> IsGitRepository(string path)
