a1aee96007eb47463acf8c40eeb513e9503bb21b
bus1hero <petru.cervac@gmail.com>
2025-01-22 22:09:38 +0200

    chore: add test to initiate repository

---
+++ Source/GitSnapshotter.UnitTests/StuffTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+
+using LibGit2Sharp;
+
+namespace GitSnapshotter.UnitTests;
+
+public class StuffTests
+{
+    [Fact]
+    public void RepositoryInitiation()
+    {
+        var path = GetTempPath();
+
+        var repo = Repository.Init(path);
+
+        using (new AssertionScope())
+        {
+            repo.Should().StartWith(path);
+            Directory.Exists(path).Should().BeTrue();
+        }
+    }
+
+    private static string GetTempPath()
+    {
+        var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        return path;
+    }
+}
