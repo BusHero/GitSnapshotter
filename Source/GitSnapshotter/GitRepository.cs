26b398881526dbbe7b13862ef9f0061f3ac8bbcd
Cervac Petru <petru.cervac@gmail.com>
2025-02-03 00:53:07 +0000

    chore: clean up dtos (#20)

--- Source/GitSnapshotter/GitRepository.cs
+++ Source/GitSnapshotter/GitRepository.cs
@@ -11,44 +11,43 @@ public class GitRepository
         var head = repo.Head.FriendlyName;
         var branches = repo.Branches
             .Where(b => !b.IsRemote)
-            .ToDictionary(
-                branch => branch.FriendlyName,
-                branch => new Branch
-                {
-                    Tip = branch.Tip.Sha,
-                    IsTracking = branch.IsTracking,
-                    TrackedBranch = branch.TrackedBranch?.FriendlyName,
-                    RemoteName = branch.RemoteName,
-                });
+            .Select(branch => new Branch
+            {
+                Tip = branch.Tip.Sha,
+                TrackedBranch = branch.TrackedBranch?.FriendlyName,
+                RemoteName = branch.RemoteName,
+                Name = branch.FriendlyName,
+            })
+            .ToArray();
 
         var remotes = repo
             .Network
             .Remotes
-            .ToDictionary(
-                remote => remote.Name,
-                remote =>
-                {
-                    var array = repo.Branches
-                        .Where(branch => branch.IsRemote)
-                        .Where(branch => branch.RemoteName.Equals(remote.Name))
-                        .ToDictionary(x => x.FriendlyName, x => x.Tip.Sha);
+            .Select(remote =>
+            {
+                var array = repo.Branches
+                    .Where(branch => branch.IsRemote)
+                    .Where(branch => branch.RemoteName.Equals(remote.Name))
+                    .ToDictionary(x => x.FriendlyName, x => x.Tip.Sha);
 
-                    return new Remote
-                    {
-                        Url = remote.Url,
-                        Branches = array,
-                    };
-                });
+                return new Remote
+                {
+                    Url = remote.Url,
+                    Branches = array,
+                    Name = remote.Name,
+                };
+            })
+            .ToArray();
 
         var tags = repo
             .Tags
-            .ToDictionary(
-                tag => tag.FriendlyName,
-                tag => new Tag
-                {
-                    Target = tag.Target.Sha,
-                    Message = tag.Annotation?.Message,
-                });
+            .Select(tag => new Tag
+            {
+                Target = tag.Target.Sha,
+                Message = tag.Annotation?.Message,
+                Name = tag.FriendlyName,
+            })
+            .ToArray();
 
         return new GitRepositorySnapshot
         {
